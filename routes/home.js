c859a6fff15c78263f61c4f8da4d4e88a5ba2a2e
Adonay Resom <azresom@wpi.edu>
2018-10-12 14:15:04 -0400

    Fixed username display and card assignments

--- routes/home.js
+++ routes/home.js
@@ -26,20 +26,29 @@ var messageSchema = new Schema({
 var Task = mongoose.model('Task', taskSchema);
 var Message = mongoose.model('Message', messageSchema);
 
-var aUser = {fullname: 'Adonay',username: '',password: '', password_confirmation:'', email: '',phone: '', venmo: ''};
+var aUser = {
+    fullname: 'Adonay',
+    username: '',
+    password: '',
+    password_confirmation: '',
+    email: '',
+    phone: '',
+    venmo: ''
+};
 var aTask = [{
     instructions: "Get me water",
     reward: "$100",
     uid_boss: "qwe",
     uid_worker: "123",
     status: "POSTED",
-    posted_time: Date.now()}];
+    posted_time: Date.now()
+}];
 var aMessage = {};
 
-function createTask(aTask ,req, res, next){
+function createTask(aTask, req, res, next) {
     console.log(JSON.stringify(aTask));
-    Task.create(aTask,function (err) {
-        if(err){
+    Task.create(aTask, function (err) {
+        if (err) {
             console.log(err.toString());
             return next(err);
         }
@@ -48,57 +57,31 @@ function createTask(aTask ,req, res, next){
     });
 }
 
-function createUser(aUser, req, res, next){
+function createUser(aUser, req, res, next) {
     var newUser = User(aUser);
     newUser.create(function (err, user) {
-        if(err) return next(err);
+        if (err) return next(err);
         console.log("User created");
         req.session.userId = user._id;
         res.redirect('/');
     });
 }
 
-function createMessage(aMessage){
+function createMessage(aMessage) {
     var newMessage = Message(aMessage);
     newMessage.save(function (err) {
-        if(err) throw err;
+        if (err) throw err;
         console.log("Message created")
     });
 }
 
-router.post('/requestjob', function (req, res, next) {
-    console.log("Received new task");
+router.post('/takejob', function (req, res, next) {
+    console.log("Taking a job");
     var dataReceived = req.body;
     Task.findById(dataReceived.tid, function (err, task) {
         if (err) throw err;
         task.uid_worker = req.session.userId;
-        task.status = "PENDING";
-        task.assigned_time = Date.now();
-        task.save();
-    });
-    renderHomePage(req, res, next);
-});
-
-router.post('/accept_pending', function (req, res, next) {
-    console.log("Received new task");
-    var dataReceived = req.body;
-    Task.findById(dataReceived.tid, function (err, task) {
-        if (err) throw err;
-        task.uid_worker = req.session.userId;
-        task.status = "PENDING";
-        task.assigned_time = Date.now();
-        task.save();
-    });
-    renderHomePage(req, res, next);
-});
-
-router.post('/reject_pending', function (req, res, next) {
-    console.log("Received new task");
-    var dataReceived = req.body;
-    Task.findById(dataReceived.tid, function (err, task) {
-        if (err) throw err;
-        task.uid_worker = req.session.userId;
-        task.status = "POSTED";
+        task.status = "ASSIGNED";
         task.assigned_time = Date.now();
         task.save();
     });
@@ -106,7 +89,7 @@ router.post('/reject_pending', function (req, res, next) {
 });
 
 router.post('/completetask', function (req, res, next) {
-    console.log("Received new task");
+    console.log("Completed task");
     var dataReceived = req.body;
     Task.findById(dataReceived.tid, function (err, task) {
         if (err) throw err;
@@ -118,8 +101,8 @@ router.post('/completetask', function (req, res, next) {
     renderHomePage(req, res, next);
 });
 
-router.post('/completepaymentk', function (req, res, next) {
-    console.log("Received new task");
+router.post('/completepayment', function (req, res, next) {
+    console.log("completed payment");
     var dataReceived = req.body;
     Task.findById(dataReceived.tid, function (err, task) {
         if (err) throw err;
@@ -134,18 +117,24 @@ router.post('/completepaymentk', function (req, res, next) {
 router.post('/newtask', function (req, res, next) {
     console.log("Received new task");
     var newTaskData = req.body;
-    var toTaskFormat = {instructions: newTaskData.new_instructions, reward: newTaskData.reward_input, uid_boss: newTaskData.uid, status: 'POSTED', posted_time: Date.now()};
+    var toTaskFormat = {
+        instructions: newTaskData.new_instructions,
+        reward: newTaskData.reward_input,
+        uid_boss: newTaskData.uid,
+        status: 'POSTED',
+        posted_time: Date.now()
+    };
     createTask(toTaskFormat, req, res, next);
 });
 
 
 router.get('/get-users', function (req, res, next) {
-        User.find({}, function (err, users) {
-            if (err) throw err;
-            res.render('home', {
-                users: users
-            });
+    User.find({}, function (err, users) {
+        if (err) throw err;
+        res.render('home', {
+            users: users
         });
+    });
 });
 
 router.get('/update-user', function (req, res, next) {
@@ -177,49 +166,60 @@ router.get('/update-user', function (req, res, next) {
 /* GET home page. */
 
 router.get('/', function (req, res, next) {
-    console.log("Found PAID");
+    console.log("Found HOME");
     renderHomePage(req, res, next, 'POSTED');
 });
-
-router.get('/pending', function (req, res, next) {
-    console.log("Found PENDING");
-    renderHomePage(req, res, next, 'PENDING');
-});
-
 router.get('/active_tasks', function (req, res, next) {
     console.log("Found ACTIVE");
     renderHomePage(req, res, next, 'ASSIGNED');
 });
-
 router.get('/completed', function (req, res, next) {
+    console.log("Found COMPLETED");
     renderHomePage(req, res, next, 'COMPLETED');
 });
-
 router.get('/paid', function (req, res, next) {
+    console.log("Found PAID");
     renderHomePage(req, res, next, 'PAID');
 });
 
 function renderHomePage(req, res, next, condition) {
-    User.find({}, function (err, users) {
+    User.find({}, '_id username fullname', function (err, users) {
         if (err) throw err;
         var user_dict = {}; // pairs uid to username inorder to avoid repetitive querying for uid username pairings later
-        for(var i = 0; i < users.length; i++){
-            user_dict[users[i].uid]=users[i].username;
-            user_dict[users[i].username]=users[i].fullname;
+        for (var i = 0; i < users.length; i++) {
+            user_dict[users[i]._id] = users[i].username;
+            user_dict[users[i].username] = users[i].fullname;
         }
         sendTasksToClient(req, res, user_dict, condition);
     });
 }
 
-function sendTasksToClient(req, res, user_dict, condition){
+function sendTasksToClient(req, res, user_dict, condition) {
     var tasksData = [];
-    Task.find({status:condition}, '*',function (err, tasks) {
+    Task.find({'status': condition}, '_id uid_boss instructions reward posted_time', function (err, tasks) {
         if (err) throw err;
-        for(var i = 0; i<tasks.length;i++){
-            tasksData.push({tid: tasks[i]._id, user_name: user_dict[tasks[i].uid_boss], instructions: tasks[i].instructions, reward: tasks[i].reward, post_time: tasks[i].posted_time});
+        for (var i = 0; i < tasks.length; i++) {
+            console.log(JSON.stringify(tasks[i]));
+            console.log("Task owned by " + tasks[i].uid_boss);
+            var taskPackage = {
+                tid: tasks[i]._id,
+                uid_boss: tasks[i].uid_boss,
+                username: user_dict[tasks[i].uid_boss],
+                fullname: user_dict[user_dict[tasks[i].uid_boss]],
+                instructions: tasks[i].instructions,
+                reward: tasks[i].reward,
+                post_time: tasks[i].posted_time
+            };
+            console.log(JSON.stringify(taskPackage));
+            tasksData.push(taskPackage);
         }
+
         console.log("Task length now: " + tasks.length);
-        var userData = {uid: req.session.userId, user_name: user_dict[req.session.userId], fullname: user_dict[user_dict[req.session.userId]]};
+        var userData = {
+            uid: req.session.userId,
+            username: user_dict[req.session.userId],
+            fullname: user_dict[user_dict[req.session.userId]]
+        };
         console.log(JSON.stringify(userData));
         console.log(JSON.stringify(tasksData));
         res.render('home', {
